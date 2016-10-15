f7de509fff58aca9c80835a7baa968b517381067
tzeth <jetoze@gmail.com>
2016-10-15 16:51:36 -0700

    Refactored out a service base class.

--- src/main/resources/public/scripts/app.js
+++ src/main/resources/public/scripts/app.js
@@ -23,46 +23,60 @@ app.config(function($routeProvider) {
 	})
 });
 
-app.service('ConditionsService', function($http, $q) {
+function GetterService($http, $q) {
+	this.$http = $http;
+	this.$q = $q;
 	this._region = 'CA';
 	this._city = 'Alameda';
-	this._conditions;
+	
+}
+
+GetterService.prototype.getRegion = function() {
+	return this._region; 
+}
+
+GetterService.prototype.getCity = function() {
+	return this._city;
+}
+
+GetterService.prototype.getData = function() {
+	return this._data;
+}
+
+GetterService.prototype.fetchData = function(region, city, api) {
+	this._region = region;
+	this._city = city;
 	var self = this;
-	
-	this.fetchConditions = function(region, city) {
-		self._region = region;
-		self._city = city;
-		var deferred = $q.defer();
-		$http.get('api/v1/conditions/' + region + '/' + city).success(function(data) {
-			deferred.resolve(data);
-			self._conditions = data;
-		}).error(function(data, status) {
-			console.log('Error: [' + status + '] ' + data);
-		});
-		return deferred.promise;
-	}
-	
-	this.getRegion = function() {
-		return self._region;
-	}
-	
-	this.getCity = function() {
-		return self._city;
-	}
-	
-	this.getConditions = function() {
-		return self._conditions;
-	}
-});
+	var deferred = this.$q.defer();
+	this.$http.get('api/v1/' + api + '/' + region + '/' + city).success(function(data) {
+		deferred.resolve(data);
+		self._data = data;
+	}).error(function(data, status) {
+		console.log('Error: [' + status + '] ' + data);
+	});
+	return deferred.promise;
+}
+
+GetterService.$inject = ['$http', '$q'];
+
+function ConditionsServiceImpl($http, $q) {
+	GetterService.call(this, $http, $q);
+}
+
+ConditionsServiceImpl.prototype = new GetterService();
+
+ConditionsServiceImpl.$inject = ['$http', '$q'];
+
+app.service('ConditionsService', ConditionsServiceImpl);
 
 app.controller('ConditionsCtrl', function($scope, ConditionsService) {
 
 	$scope.region = ConditionsService.getRegion();
 	$scope.city = ConditionsService.getCity();
-	$scope.conditions = ConditionsService.getConditions();
+	$scope.conditions = ConditionsService.getData();
 
 	$scope.fetchConditions = function() {
-		ConditionsService.fetchConditions($scope.region, $scope.city).then(function(c) {
+		ConditionsService.fetchData($scope.region, $scope.city, 'conditions').then(function(c) {
 			$scope.conditions = c
 		});
 	};
